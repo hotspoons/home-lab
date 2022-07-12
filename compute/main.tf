a669e1532a87fd458a87ef31d8622af9656f9b75
root <root@vm-host.siomporas.com>
2022-07-12 00:01:01 -0400

    Got it working, time for bedgit add main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -14,19 +14,19 @@ locals{
   memory         = "4294967296"
   maximum_memory = "6442450944"
   domain_suffix  = "siomporas.com"
-  root_password  = "password"
+  root_password  = "root"
   cpu_cores      = "4"
   cpu_sockets    = "1"
   cpu_threads    = "2"
   template_id    = "aceb058e-5689-49d3-a9d6-4caae908e34c"
-  initialization_custom_script = <<-EOT
-#!/bin/bash
-echo &apos;${local.root_password}&apos; | passwd --stdin root
-cd /tmp
-echo &apos;asdf&apos; &gt; index.html
-#python3 -m http.server 8000&amp;
-EOT
-  
+  initialization_custom_script = yamlencode({
+    "ssh_authorized_keys": [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDQIwABScaeMJIA8y20PeQsaGvE5mvgetDepksBoOUKHOsbFxcYwdnPb2Xu3FUTOs38heyi4FgUhsSRxTDSI2VFDyNsR0WugXAwhkPHu94QXc3OzPegAGUW4b6+CNwtd7Me/BvqBSCtRLWhoMc3FPD/gA51pTCmnqh1PnqcueEGEufw7I2+zMK0lxB5/LjQmGBmN9pgTLKuHFYMs3Ywfk7JmWGzWr8v2AjIGbJgi2fdO7XjqoFlaJ4w21StZhxevFy+Im1pwGXlHyHTEuG6diwsf/fg9lWwImVE0ntwuajJi19kwOEUbcsq4REk/gWp+S2/yAB2waH7SGqLlGdfbuCMyie71nxRelKv/8YDabybWD7d2u6FvcEXyWYJeyAXFu5L6vPsytAyChl+7aikk6Qmpzx3O/HSR6Hx83w57Elc2UOK/LJ1UVQZgiuw4O6WReL+U7lK8fvn3xZSYFOLFG9Ga82l0yJlY2M7hnaOhUG8v4jCnGTKX7FTGHiFihx1cgk= rich@rich-xp-new"
+      ]
+    "runcmd": [
+      "echo &#39;${local.root_password}&#39; | passwd --stdin root"
+      ]
+  })
 }
 
 resource "ovirt_vm" "k8s-master" {
