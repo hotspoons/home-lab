f7e98ad0e1dbbc3f66631b4053d98b1ffb344bb0
root <root@vm-host.siomporas.com>
2022-07-12 10:39:58 -0400

    Refactored config

--- compute/main.tf
+++ compute/main.tf
@@ -10,29 +10,24 @@ terraform {
 }
 
 locals{
-  cluster_id     = "c0769f3c-9c03-11ec-bc0d-00163e448789"
-  memory         = "4294967296"
-  maximum_memory = "6442450944"
-  domain_suffix  = "siomporas.com"
-  root_password  = "root"
-  cpu_cores      = "4"
-  cpu_sockets    = "1"
-  cpu_threads    = "2"
-  template_id    = "aceb058e-5689-49d3-a9d6-4caae908e34c"
+  cluster_id     = var.cluster_id
+  memory         = var.memory
+  maximum_memory = var.maximum_memory
+  domain_suffix  = var.domain_suffix
+  cpu_cores      = var.cpu_cores
+  cpu_sockets    = var.cpu_sockets
+  cpu_threads    = var.cpu_threads
+  template_id    = var.template_id
   initialization_custom_script = yamlencode({
-    "ssh_authorized_keys": [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDQIwABScaeMJIA8y20PeQsaGvE5mvgetDepksBoOUKHOsbFxcYwdnPb2Xu3FUTOs38heyi4FgUhsSRxTDSI2VFDyNsR0WugXAwhkPHu94QXc3OzPegAGUW4b6+CNwtd7Me/BvqBSCtRLWhoMc3FPD/gA51pTCmnqh1PnqcueEGEufw7I2+zMK0lxB5/LjQmGBmN9pgTLKuHFYMs3Ywfk7JmWGzWr8v2AjIGbJgi2fdO7XjqoFlaJ4w21StZhxevFy+Im1pwGXlHyHTEuG6diwsf/fg9lWwImVE0ntwuajJi19kwOEUbcsq4REk/gWp+S2/yAB2waH7SGqLlGdfbuCMyie71nxRelKv/8YDabybWD7d2u6FvcEXyWYJeyAXFu5L6vPsytAyChl+7aikk6Qmpzx3O/HSR6Hx83w57Elc2UOK/LJ1UVQZgiuw4O6WReL+U7lK8fvn3xZSYFOLFG9Ga82l0yJlY2M7hnaOhUG8v4jCnGTKX7FTGHiFihx1cgk= rich@rich-xp-new"
-      ]
-    "runcmd": [
-      "echo &#39;${local.root_password}&#39; | passwd --stdin root"
-      ]
+    "ssh_authorized_keys": var.ssh_authorized_keys
+    "runcmd": var.initialization_commands
   })
 }
 
 resource "ovirt_vm" "k8s-master" {
   cluster_id     = local.cluster_id
-  name           = "k8s-master.${local.domain_suffix}"
-  initialization_hostname = "k8s-master.${local.domain_suffix}"
+  name           = "k8s-master${local.domain_suffix}"
+  initialization_hostname = "k8s-master${local.domain_suffix}"
   initialization_custom_script = local.initialization_custom_script
   memory         = local.memory
   maximum_memory = local.maximum_memory
@@ -43,8 +38,8 @@ resource "ovirt_vm" "k8s-master" {
 }
 resource "ovirt_vm" "k8s-node1" {
   cluster_id     = local.cluster_id
-  name       = "k8s-node1.${local.domain_suffix}"
-  initialization_hostname = "k8s-node1.${local.domain_suffix}"
+  name       = "k8s-node1${local.domain_suffix}"
+  initialization_hostname = "k8s-node1${local.domain_suffix}"
   initialization_custom_script = local.initialization_custom_script
   memory         = local.memory
   maximum_memory = local.maximum_memory
@@ -55,8 +50,8 @@ resource "ovirt_vm" "k8s-node1" {
 }
 resource "ovirt_vm" "k8s-node2" {
   cluster_id     = local.cluster_id
-  name            = "k8s-node2.${local.domain_suffix}"
-  initialization_hostname = "k8s-node2.${local.domain_suffix}"
+  name            = "k8s-node2${local.domain_suffix}"
+  initialization_hostname = "k8s-node2${local.domain_suffix}"
   initialization_custom_script = local.initialization_custom_script
   memory         = local.memory
   maximum_memory = local.maximum_memory
--- compute/vars.tf
+++ compute/vars.tf
@@ -1,43 +1,106 @@
+#Cluster-level vars
 variable "url" {
     description = "oVirt API URL"
-    default = "https://vm-manager.siomporas.com/ovirt-engine/api"
+    type = string
 }
 variable "username" {
     description = "oVirt Admin user"
-    default     = "admin@internal"
+    type = string
 }
 variable "password" {
     description = "oVirt Admin password"
-    default     = "R@R@1134"
+    type = string
 }
 
 variable "tls_ca_files" {
-    description = "tls_ca_files"
-    default     = ""
+    description = "CA Files for TLS"
+    type = string
+    default = ""
 }
 
 variable "tls_ca_dirs" {
-    description = "tls_ca_dirs"
-    default     = ""
+    description = "CA FIle directory for TLS"
+    type = string
+    default = ""
 }
 
 variable "tls_ca_bundle" {
-    description = "tls_ca_bundle"
-    default     = ""
+    description = "CA Bundle for TLS"
+    type = string
+    default = ""
 }
 
 variable "tls_system" {
     description = "tls_system"
-    default     = ""
+    type = string
+    default = ""
 }
 
 variable "tls_insecure" {
-    description = "tls_insecure"
-    default     = "true"
+    description = "Allow insecure TLS, true or false, defaults to false"
+    type = string
+    default = "false"
 }
 
 variable "mock" {
-    description = "mock"
-    default     = "false"
+    description = "Run with a mock system, defaults to false"
+    type = string
+    default = "false"
+}
+
+variable "cluster_id" {
+    description = "The ID of the cluster"
+    type = string
+}
+
+#Compute-level vars
+
+variable "memory" {
+    description = "The amount of guranteed RAM for each compute node, in bytes. Defaults to 2GB (in bytes)"
+    type = string
+    default = "2147483648"
+}
+
+variable "maximum_memory" {
+    description = "The maximum amount of RAM for each compute node, in bytes. Defaults to 2GB (in bytes)"
+    type = string
+    default = "2147483648"
+}
+
+variable "domain_suffix" {
+    description = "A domain suffix for all VMs, starting with the leading . Defaults to empty"
+    type = string
+    default = ""
+}
+
+variable "cpu_cores" {
+    description = "Number of CPU cores, defaults to 2"
+    type = string
+    default = "2"
+}
+
+variable "cpu_sockets" {
+    description = "Number of CPU sockets, defaults to 1"
+    default = "1"
+}
+
+variable "cpu_threads" {
+    description = "CPU threads, defaults to 1"
+    default = "1"
+}
+
+variable "initialization_commands" {
+    description = "A list of commands to be executed on first start, such as resetting a password - all commands must be XML encoded!"
+    type = list(string)
+    default = ["echo &#39;works&#39;"]
+}
+
+variable "ssh_authorized_keys" {
+    description = "A list of ssh authorized keys to be installed on the new VM"
+    type = list(string)
+}
+
+variable "template_id" {
+    description = "The template ID from your oVirt install you wish to use as the baseline for your compute"
 }
 
