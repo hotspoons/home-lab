434b9f9f5c7aa8cab9fd4206f6ed28ac924a16a7
root <root@vm-host.siomporas.com>
2022-07-11 23:25:30 -0400

    working through encoding issues with module

--- compute/main.tf
+++ compute/main.tf
@@ -9,47 +9,76 @@ terraform {
   }
 }
 
-resource "ovirt_vm" "k8s-master" {
+locals{
   cluster_id     = "c0769f3c-9c03-11ec-bc0d-00163e448789"
-  name           = "k8s-master.siomporas.com"
-  initialization_hostname = "k8s-master.siomporas.com"
   memory         = "4294967296"
   maximum_memory = "6442450944"
+  domain_suffix  = "siomporas.com"
+  root_password  = "password"
   cpu_cores      = "4"
   cpu_sockets    = "1"
   cpu_threads    = "2"
   template_id    = "aceb058e-5689-49d3-a9d6-4caae908e34c"
+  initialization_custom_script = <<-EOT
+#!/bin/bash
+echo &apos;${local.root_password}&apos; | passwd --stdin root
+cd /tmp
+echo &apos;asdf&apos; &gt; index.html
+#python3 -m http.server 8000&amp;
+EOT
+  
+}
+
+resource "ovirt_vm" "k8s-master" {
+  cluster_id     = local.cluster_id
+  name           = "k8s-master.${local.domain_suffix}"
+  initialization_hostname = "k8s-master.${local.domain_suffix}"
+  initialization_custom_script = local.initialization_custom_script
+  memory         = local.memory
+  maximum_memory = local.maximum_memory
+  cpu_cores      = local.cpu_cores
+  cpu_sockets    = local.cpu_sockets
+  cpu_threads    = local.cpu_threads
+  template_id    = local.template_id
 }
 resource "ovirt_vm" "k8s-node1" {
-  cluster_id        = "c0769f3c-9c03-11ec-bc0d-00163e448789"
-  name       = "k8s-node1.siomporas.com"
-  initialization_hostname = "k8s-node1.siomporas.com"
-  memory         = "4294967296"
-  maximum_memory     = "6442450944"
-  cpu_cores      = "4"
-  cpu_sockets    = "1"
-  cpu_threads    = "2"
-  template_id    = "aceb058e-5689-49d3-a9d6-4caae908e34c"
+  cluster_id     = local.cluster_id
+  name       = "k8s-node1.${local.domain_suffix}"
+  initialization_hostname = "k8s-node1.${local.domain_suffix}"
+  initialization_custom_script = local.initialization_custom_script
+  memory         = local.memory
+  maximum_memory = local.maximum_memory
+  cpu_cores      = local.cpu_cores
+  cpu_sockets    = local.cpu_sockets
+  cpu_threads    = local.cpu_threads
+  template_id    = local.template_id
 }
 resource "ovirt_vm" "k8s-node2" {
-  cluster_id        = "c0769f3c-9c03-11ec-bc0d-00163e448789"
-  name            = "k8s-node2.siomporas.com"
-  initialization_hostname = "k8s-node2.siomporas.com"
-  memory         = "4294967296"
-  maximum_memory     = "6442450944"
-  cpu_cores      = "4"
-  cpu_sockets    = "1"
-  cpu_threads    = "2"
-  template_id    = "aceb058e-5689-49d3-a9d6-4caae908e34c"
+  cluster_id     = local.cluster_id
+  name            = "k8s-node2.${local.domain_suffix}"
+  initialization_hostname = "k8s-node2.${local.domain_suffix}"
+  initialization_custom_script = local.initialization_custom_script
+  memory         = local.memory
+  maximum_memory = local.maximum_memory
+  cpu_cores      = local.cpu_cores
+  cpu_sockets    = local.cpu_sockets
+  cpu_threads    = local.cpu_threads
+  template_id    = local.template_id
 }
 
 resource "ovirt_vm_start" "k8s-master" {
   vm_id = ovirt_vm.k8s-master.id
+  stop_behavior = "stop"
+  force_stop = true
 }
 resource "ovirt_vm_start" "k8s-node1" {
   vm_id = ovirt_vm.k8s-node1.id
+  stop_behavior = "stop"
+  force_stop = true
 }
 resource "ovirt_vm_start" "k8s-node2" {
   vm_id = ovirt_vm.k8s-node2.id
+  stop_behavior = "stop"
+  force_stop = true
 }
 
