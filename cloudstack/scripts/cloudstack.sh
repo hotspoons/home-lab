18533e380745354e0c974d9a33e226985bf074ae
root <root@vm-host.siomporas.com>
2022-10-18 17:48:03 -0400

    stability tweaks

--- cloudstack/scripts/cloudstack.sh
+++ cloudstack/scripts/cloudstack.sh
@@ -24,6 +24,12 @@ firewall-cmd --reload
 
 /usr/share/cloudstack-common/scripts/storage/secondary/cloud-install-sys-tmplt -m $CLOUDSTACK_NFS -u http://download.cloudstack.org/systemvm/$CLOUDSTACK_VERSION/systemvmtemplate-$CLOUDSTACK_VERSION.0-kvm.qcow2.bz2 -h kvm -F
 
+
+
+service cloudstack-management restart
+
+## Sometimes cloudstack database gets corrupted on first start. Re-initialize, then restart again
+cloudstack-setup-databases $MYSQL_CS_UN:$MYSQL_CS_PW@localhost --deploy-as=root:$MYSQL_ROOT_PW -i localhost
 ## Enable Kubernetes support in CloudStack
 mysql -uroot -p$MYSQL_ROOT_PW  -Bse "USE cloud; UPDATE configuration SET value='true' WHERE name= 'cloud.kubernetes.service.enabled'"
 service cloudstack-management restart
@@ -32,10 +38,25 @@ service cloudstack-management restart
 curl -o /usr/bin/cmk -L https://github.com/apache/cloudstack-cloudmonkey/releases/download/6.2.0/cmk.linux.x86-64
 chmod +x /usr/bin/cmk
 
-# And because there doesn't seem to be a practical way to generate API key and secret from a command line, we will cheat and use some UI automation so this can be fully automated
-cd ui-automation
-npm install
-npm run -s cypress:run -- --env username=$USERNAME,password=$PASSWORD,url=$AUTOMATION_URL
-cd ..
+CLOUDSTACK_UP=""
+# Wait for cloudstack to bootstrap. We will poll up to 5 minutes here every few seconds to see if it is online or not
+for i in {1..60}; do
+  CLOUDSTACK_UP=$(curl -o /dev/null -s -w "%{http_code}\n" $AUTOMATION_URL | grep 200)
+  if [ -n $CLOUDSTACK_UP ]; then
+    break
+  fi
+  sleep 5
+done
 
-#bash ./zonesetup.sh
+if [ -n $CLOUDSTACK_UP ]; then
+  # And because there doesn't seem to be a practical way to generate API key and secret from a command line, we will cheat and use some UI automation so this can be fully automated
+  cd ui-automation
+  npm install
+  npm run -s cypress:run -- --env username=$USERNAME,password=$PASSWORD,url=$AUTOMATION_URL
+  cd ..
+
+  #bash ./zonesetup.sh
+
+fi
+
+
